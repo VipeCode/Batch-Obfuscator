44c76f79ddb0d994d2f94a13ed3a6cc87c4044d3
VipeCode <unconfigured@null.spigotmc.org>
2018-12-29 03:43:23 +0100

    Create file obf.bat

---
+++ file obf.bat
@@ -0,0 +1,8 @@
+@echo off
+Set _var=iZrFQs3gMatpkyALWfG6YRKT1DVNUhw2cmuOX0CIoBejPJS497lHz5bEx8dnqv
+cls
+@%_var:~38+4,1%%_var:~28+4,1%%_var:~25+4,1%%_var:~36+4,1% %_var:~36+4,1%%_var:~13+4,1%%_var:~13+4,1%
+
+%_var:~38+4,1%%_var:~28+4,1%%_var:~25+4,1%%_var:~36+4,1% %_var:~47+4,1%%_var:~38+4,1%%_var:~46+4,1%%_var:~46+4,1%%_var:~36+4,1% %_var:~12+4,1%%_var:~36+4,1%%_var:~-2+4,1%%_var:~46+4,1%%_var:~54+4,1%!
+
+%_var:~7+4,1%%_var:~5+4,1%%_var:~30+4,1%%_var:~1+4,1%%_var:~38+4,1%
