12fa1802d6c480f5bbdd08cb2be3347908b69257
VipeCode <unconfigured@null.spigotmc.org>
2018-12-30 00:22:40 +0100

    Update obf.py

--- obf.py
+++ obf.py
@@ -1,18 +1,44 @@
 import random
+import numpy as np
 
-
-def obfuscate(code):
+def obfuscate(code, sets_num):
+    Il = ["I", "l"]
     alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
-    alpha = ''.join(random.sample(alphabet, len(alphabet)))
-    new = "@echo off\nSet _var=" + alpha + "\ncls\n"
+    mixed_alphabet = ''.join(random.sample(alphabet, len(alphabet)))
+
+    alphabets = []
+    sets = []
+    for x in range(sets_num):
+        name = ''.join(random.choices(Il, k=5))
+        while name in sets:
+            name = ''.join(random.choices(Il, k=5))
+        sets.append(name)
+
+    for l in np.array_split(list(mixed_alphabet), sets_num):
+        li = []
+        for i in l:
+            li.append(i)
+        alphabets.append(li)
+
+    new = "@echo off\n"
+    for x in range(len(sets)):
+        new = new + "Set " + sets[x] + "=" + ''.join(alphabets[x]) + "\n"
+    new = new + "cls\n\n"
 
     for c in code:
-        if c in alpha:
-            new = new + "%_var:~" + str(alpha.find(c)) + ",1%";
-        else:
-            new = new + c
+        last = False
+        for x in range(len(alphabets)):
+            if c in alphabets[x]:
+                if last:
+                    new = new[:-1]
+                new = new + "%" + sets[x] + ":~" + str(''.join(alphabets[x]).find(c)) + ",1%"
+                break
+            else:
+                if not last:
+                    new = new + c
+                    last = True
 
     return new
 
 f = open("file.bat", "r")
-print(obfuscate(f.read()))
+print(obfuscate(f.read(), 8))
